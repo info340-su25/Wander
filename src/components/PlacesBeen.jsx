d09d9e0ab6c1f3cd3e6f8db373d7d99784aab238
hannapan8 <hzpan8@uw.edu>
2025-08-05 16:15:32 -0700

    Adds footer component to pages

--- src/components/PlacesBeen.jsx
+++ src/components/PlacesBeen.jsx
@@ -45,13 +45,7 @@ function PlacesBeen() {
 
             </main>
 
-            <footer className="bottom-nav">
-                <a href="index.html"><span className="material-symbols-outlined">home</span></a>
-                <a href="view-trips.html"><span className="material-symbols-outlined">format_list_bulleted</span></a>
-                <a href="create-new-log.html"><span className="material-symbols-outlined">add_box</span></a>
-                <a href="view-map.html"><span className="material-symbols-outlined">map</span></a>
-                <a href="profile.html"><span className="material-symbols-outlined">account_circle</span></a>
-            </footer>
+            <Footer />
         </div>
     );
 }
--- src/components/ViewMap.jsx
+++ src/components/ViewMap.jsx
@@ -49,13 +49,7 @@ function ViewMap() {
                 </div>
             </main>
 
-            <footer className="view-bottom-nav">
-                <a href="index.html"><span className="material-symbols-outlined">home</span></a>
-                <a href="view-trips.html"><span className="material-symbols-outlined">format_list_bulleted</span></a>
-                <a href="create-new-log.html"><span className="material-symbols-outlined">add_box</span></a>
-                <a href="view-map.html"><span className="material-symbols-outlined">map</span></a>
-                <a href="profile.html"><span className="material-symbols-outlined">account_circle</span></a>
-            </footer>
+            <Footer />
         </div>
     );
 }
