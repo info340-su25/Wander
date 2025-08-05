1f448a7aa2169b7172d8eecbdbff29a1e836f947
Nicole Ham <nicole.xi.ham@gmail.com>
2025-08-05 16:47:35 -0700

    made search bar more reactive

--- src/components/index/SearchBar.jsx
+++ src/components/index/SearchBar.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchBar() {
+    const [search, setSearch] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Search submitted:", search);
+        // Future thing to worry about :D
+    };
+
+    // return (
+    //     <section className="search-bar">
+    //         <span className="material-symbols-outlined">search</span>
+    //         <p>Search a travel destination, attraction, etc.</p>
+    //     </section>
+    // );
+
+    // This is broken, need to find a way to make this not break 
     return (
         <section className="search-bar">
-            <span className="material-symbols-outlined">search</span>
-            <p>Search a travel destination, attraction, etc.</p>
+            <form onSubmit={handleSubmit}>
+                <span className="material-symbols-outlined">search</span>
+                <input
+                    type="text"
+                    placeholder="Search a travel destination, attraction, etc."
+                    value={search}
+                    onChange={(event) => setSearch(event.target.value)}
+                />
+            </form>
         </section>
     );
 }
